eab822d992d3066445c06650494c139ced0a33ee
taont-wq <tao.nguyentrong@gmail.com>
2026-07-29 16:32:16 +0700

    Add chap.js, language field, icon in zip, fix regexp escaping for vBook beta

---
+++ eporner/src/chap.js
@@ -0,0 +1,15 @@
+function execute(url) {
+  var doc = fetch(url).html();
+  var title = doc.select("h1").text();
+  if (!title) {
+    title = doc.select("meta[property='og:title']").attr("content");
+  }
+  if (!title) {
+    title = "Video";
+  }
+  return Response.success({
+    title: title,
+    content: url,
+    host: "https://www.eporner.com"
+  });
+}
